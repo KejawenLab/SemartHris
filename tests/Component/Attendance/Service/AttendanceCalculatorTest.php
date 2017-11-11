a631a3ca2277dc07aad00863793f85283ba33d8c
Muhamad Surya Iksanudin <surya.kejawen@gmail.com>
2017-11-12 06:18:00 +0700

    add more test

--- tests/Component/Attendance/Service/AttendanceCalculatorTest.php
+++ tests/Component/Attendance/Service/AttendanceCalculatorTest.php
@@ -15,27 +15,274 @@ use PHPUnit\Framework\TestCase;
  */
 class AttendanceCalculatorTest extends TestCase
 {
+    /**
+     * @var EmployeeInterface
+     */
+    private $employee;
+
+    /**
+     * @var \DateTimeInterface
+     */
+    private $attendanceDate;
+
+    /**
+     * @var \MockTestInterface
+     */
+    private $shiftment;
+
+    /**
+     * @var WorkshiftInterface
+     */
+    private $workshift;
+
+    /**
+     * @var WorkshiftRepositoryInterface
+     */
+    private $workshiftRepository;
+
+    public function setUp()
+    {
+        $this->employee = $this->getMockBuilder(EmployeeInterface::class)->getMock();
+        $this->attendanceDate = new \DateTime();
+
+        $this->shiftment = $this->getMockBuilder(ShiftmentInterface::class)->getMock();
+
+        $this->workshift = $this->getMockBuilder(WorkshiftInterface::class)->getMock();
+        $this->workshift->expects($this->once())->method('getShiftment')->willReturn($this->shiftment);
+
+        $this->workshiftRepository = $this->getMockBuilder(WorkshiftRepositoryInterface::class)->getMock();
+        $this->workshiftRepository->expects($this->once())->method('findByEmployeeAndDate')->with($this->employee, $this->attendanceDate)->willReturn($this->workshift);
+    }
+
     public function testAbsent()
     {
-        $employee = $this->getMockBuilder(EmployeeInterface::class)->getMock();
-        $date = new \DateTime();
-
-        $shiftment = $this->getMockBuilder(ShiftmentInterface::class)->getMock();
-
-        $workshift = $this->getMockBuilder(WorkshiftInterface::class)->getMock();
-        $workshift->expects($this->once())->method('getShiftment')->willReturn($shiftment);
-
-        $workshiftRepository = $this->getMockBuilder(WorkshiftRepositoryInterface::class)->getMock();
-        $workshiftRepository->expects($this->once())->method('findByEmployeeAndDate')->with($employee, $date)->willReturn($workshift);
-
         $attendance = $this->getMockBuilder(AttendanceInterface::class)->getMock();
-        $attendance->expects($this->once())->method('getEmployee')->willReturn($employee);
-        $attendance->expects($this->once())->method('getAttendanceDate')->willReturn($date);
-        $attendance->expects($this->once())->method('setShiftment')->with($shiftment);
+        $attendance->expects($this->once())->method('getEmployee')->willReturn($this->employee);
+        $attendance->expects($this->once())->method('getAttendanceDate')->willReturn($this->attendanceDate);
+        $attendance->expects($this->once())->method('setShiftment')->with($this->shiftment);
         $attendance->expects($this->once())->method('isAbsent')->willReturn(true);
         $attendance->expects($this->never())->method('setReason');
 
-        $calculator = new AttendanceCalculator($workshiftRepository);
+        $calculator = new AttendanceCalculator($this->workshiftRepository);
         $calculator->calculate($attendance);
+    }
+
+    public function testNotCheckInAndCheckout()
+    {
+        $attendance = $this->getMockBuilder(AttendanceInterface::class)->getMock();
+        $attendance->expects($this->once())->method('getEmployee')->willReturn($this->employee);
+        $attendance->expects($this->once())->method('getAttendanceDate')->willReturn($this->attendanceDate);
+        $attendance->expects($this->once())->method('setShiftment')->with($this->shiftment);
+        $attendance->expects($this->once())->method('isAbsent')->willReturn(false);
+        $attendance->expects($this->once())->method('getCheckIn')->willReturn(null);
+        $attendance->expects($this->once())->method('getCheckOut')->willReturn(null);
+        $attendance->expects($this->never())->method('setReason');
+
+        $calculator = new AttendanceCalculator($this->workshiftRepository);
+        $calculator->calculate($attendance);
+    }
+
+    public function testWorkInWithoutCheckIn()
+    {
+        $attendance = $this->getMockBuilder(AttendanceInterface::class)->getMock();
+        $attendance->expects($this->once())->method('getEmployee')->willReturn($this->employee);
+        $attendance->expects($this->once())->method('getAttendanceDate')->willReturn($this->attendanceDate);
+        $attendance->expects($this->once())->method('setShiftment')->with($this->shiftment);
+        $attendance->expects($this->once())->method('isAbsent')->willReturn(false);
+        $attendance->expects($this->atLeastOnce())->method('getCheckIn')->willReturn(null);
+        $attendance->expects($this->atLeastOnce())->method('getCheckOut')->willReturn($this->attendanceDate);
+        $attendance->expects($this->once())->method('setReason');
+
+        $this->setUpShiftment($this->attendanceDate, $this->attendanceDate);
+
+        $calculator = new AttendanceCalculator($this->workshiftRepository);
+        $calculator->calculate($attendance);
+    }
+
+    public function testAttendanceCalculatorWithoutCheckout()
+    {
+        $checkIn = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 08:00:00', $this->attendanceDate->format('Y-m-d')));
+        $checkOut = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 17:00:00', $this->attendanceDate->format('Y-m-d')));
+
+        $this->setUpShiftment($checkIn, $checkOut);
+
+        $attendance = new AttendanceStub();
+        $attendance->setEmployee($this->employee);
+        $attendance->setShiftment($this->shiftment);
+        $attendance->setAttendanceDate($this->attendanceDate);
+        $attendance->setCheckIn($checkIn);
+
+        $calculator = new AttendanceCalculator($this->workshiftRepository);
+        $calculator->calculate($attendance);
+
+        $this->assertEquals(0, $attendance->getEarlyIn());
+        $this->assertEquals(0, $attendance->getEarlyOut());
+        $this->assertEquals(0, $attendance->getLateIn());
+        $this->assertEquals(0, $attendance->getLateOut());
+        $this->assertEquals($checkOut->getTimestamp(), $attendance->getCheckOut()->getTimestamp());
+    }
+
+    public function testAttendanceCalculatorWithoutCheckIn()
+    {
+        $checkIn = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 08:00:00', $this->attendanceDate->format('Y-m-d')));
+        $checkOut = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 17:00:00', $this->attendanceDate->format('Y-m-d')));
+
+        $this->setUpShiftment($checkIn, $checkOut);
+
+        $attendance = new AttendanceStub();
+        $attendance->setEmployee($this->employee);
+        $attendance->setShiftment($this->shiftment);
+        $attendance->setAttendanceDate($this->attendanceDate);
+        $attendance->setCheckOut($checkOut);
+
+        $calculator = new AttendanceCalculator($this->workshiftRepository);
+        $calculator->calculate($attendance);
+
+        $this->assertEquals(0, $attendance->getEarlyIn());
+        $this->assertEquals(0, $attendance->getEarlyOut());
+        $this->assertEquals(0, $attendance->getLateIn());
+        $this->assertEquals(0, $attendance->getLateOut());
+        $this->assertEquals($checkIn->getTimestamp(), $attendance->getCheckIn()->getTimestamp());
+    }
+
+    public function testAttendanceCalculatorEaryIn()
+    {
+        $startHour = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 08:00:00', $this->attendanceDate->format('Y-m-d')));
+        $checkIn = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 07:30:00', $this->attendanceDate->format('Y-m-d')));
+        $endHour = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 17:00:00', $this->attendanceDate->format('Y-m-d')));
+
+        $this->setUpShiftment($startHour, $endHour);
+
+        $attendance = new AttendanceStub();
+        $attendance->setEmployee($this->employee);
+        $attendance->setShiftment($this->shiftment);
+        $attendance->setAttendanceDate($this->attendanceDate);
+        $attendance->setCheckIn($checkIn);
+        $attendance->setCheckOut($endHour);
+
+        $calculator = new AttendanceCalculator($this->workshiftRepository);
+        $calculator->calculate($attendance);
+
+        $this->assertEquals(30, $attendance->getEarlyIn());
+        $this->assertEquals(0, $attendance->getEarlyOut());
+        $this->assertEquals(0, $attendance->getLateIn());
+        $this->assertEquals(0, $attendance->getLateOut());
+        $this->assertEquals($checkIn->getTimestamp(), $attendance->getCheckIn()->getTimestamp());
+        $this->assertEquals($endHour->getTimestamp(), $attendance->getCheckOut()->getTimestamp());
+    }
+
+    public function testAttendanceCalculatorLateIn()
+    {
+        $startHour = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 08:00:00', $this->attendanceDate->format('Y-m-d')));
+        $checkIn = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 08:15:00', $this->attendanceDate->format('Y-m-d')));
+        $endHour = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 17:00:00', $this->attendanceDate->format('Y-m-d')));
+
+        $this->setUpShiftment($startHour, $endHour);
+
+        $attendance = new AttendanceStub();
+        $attendance->setEmployee($this->employee);
+        $attendance->setShiftment($this->shiftment);
+        $attendance->setAttendanceDate($this->attendanceDate);
+        $attendance->setCheckIn($checkIn);
+        $attendance->setCheckOut($endHour);
+
+        $calculator = new AttendanceCalculator($this->workshiftRepository);
+        $calculator->calculate($attendance);
+
+        $this->assertEquals(0, $attendance->getEarlyIn());
+        $this->assertEquals(0, $attendance->getEarlyOut());
+        $this->assertEquals(15, $attendance->getLateIn());
+        $this->assertEquals(0, $attendance->getLateOut());
+        $this->assertEquals($checkIn->getTimestamp(), $attendance->getCheckIn()->getTimestamp());
+        $this->assertEquals($endHour->getTimestamp(), $attendance->getCheckOut()->getTimestamp());
+    }
+
+    public function testAttendanceCalculatorEaryOut()
+    {
+        $startHour = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 08:00:00', $this->attendanceDate->format('Y-m-d')));
+        $endHour = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 17:00:00', $this->attendanceDate->format('Y-m-d')));
+        $checkOut = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 16:45:00', $this->attendanceDate->format('Y-m-d')));
+
+        $this->setUpShiftment($startHour, $endHour);
+
+        $attendance = new AttendanceStub();
+        $attendance->setEmployee($this->employee);
+        $attendance->setShiftment($this->shiftment);
+        $attendance->setAttendanceDate($this->attendanceDate);
+        $attendance->setCheckIn($startHour);
+        $attendance->setCheckOut($checkOut);
+
+        $calculator = new AttendanceCalculator($this->workshiftRepository);
+        $calculator->calculate($attendance);
+
+        $this->assertEquals(0, $attendance->getEarlyIn());
+        $this->assertEquals(15, $attendance->getEarlyOut());
+        $this->assertEquals(0, $attendance->getLateIn());
+        $this->assertEquals(0, $attendance->getLateOut());
+        $this->assertEquals($startHour->getTimestamp(), $attendance->getCheckIn()->getTimestamp());
+        $this->assertEquals($checkOut->getTimestamp(), $attendance->getCheckOut()->getTimestamp());
+    }
+
+    public function testAttendanceCalculatorLateOut()
+    {
+        $startHour = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 08:00:00', $this->attendanceDate->format('Y-m-d')));
+        $endHour = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 17:00:00', $this->attendanceDate->format('Y-m-d')));
+        $checkOut = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 17:45:00', $this->attendanceDate->format('Y-m-d')));
+
+        $this->setUpShiftment($startHour, $endHour);
+
+        $attendance = new AttendanceStub();
+        $attendance->setEmployee($this->employee);
+        $attendance->setShiftment($this->shiftment);
+        $attendance->setAttendanceDate($this->attendanceDate);
+        $attendance->setCheckIn($startHour);
+        $attendance->setCheckOut($checkOut);
+
+        $calculator = new AttendanceCalculator($this->workshiftRepository);
+        $calculator->calculate($attendance);
+
+        $this->assertEquals(0, $attendance->getEarlyIn());
+        $this->assertEquals(0, $attendance->getEarlyOut());
+        $this->assertEquals(0, $attendance->getLateIn());
+        $this->assertEquals(45, $attendance->getLateOut());
+        $this->assertEquals($startHour->getTimestamp(), $attendance->getCheckIn()->getTimestamp());
+        $this->assertEquals($checkOut->getTimestamp(), $attendance->getCheckOut()->getTimestamp());
+    }
+
+    public function testAttendanceCalculatorEaryInLateOut()
+    {
+        $startHour = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 08:00:00', $this->attendanceDate->format('Y-m-d')));
+        $checkIn = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 07:30:00', $this->attendanceDate->format('Y-m-d')));
+        $endHour = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 17:00:00', $this->attendanceDate->format('Y-m-d')));
+        $checkOut = \DateTime::createFromFormat('Y-m-d H:i:s', sprintf('%s 17:45:00', $this->attendanceDate->format('Y-m-d')));
+
+        $this->setUpShiftment($startHour, $endHour);
+
+        $attendance = new AttendanceStub();
+        $attendance->setEmployee($this->employee);
+        $attendance->setShiftment($this->shiftment);
+        $attendance->setAttendanceDate($this->attendanceDate);
+        $attendance->setCheckIn($checkIn);
+        $attendance->setCheckOut($checkOut);
+
+        $calculator = new AttendanceCalculator($this->workshiftRepository);
+        $calculator->calculate($attendance);
+
+        $this->assertEquals(30, $attendance->getEarlyIn());
+        $this->assertEquals(0, $attendance->getEarlyOut());
+        $this->assertEquals(0, $attendance->getLateIn());
+        $this->assertEquals(45, $attendance->getLateOut());
+        $this->assertEquals($checkIn->getTimestamp(), $attendance->getCheckIn()->getTimestamp());
+        $this->assertEquals($checkOut->getTimestamp(), $attendance->getCheckOut()->getTimestamp());
+    }
+
+    /**
+     * @param \DateTimeInterface $startHour
+     * @param \DateTimeInterface $endHour
+     */
+    private function setUpShiftment(\DateTimeInterface $startHour, \DateTimeInterface $endHour)
+    {
+        $this->shiftment->expects($this->atLeastOnce())->method('getStartHour')->willReturn($startHour);
+        $this->shiftment->expects($this->atLeastOnce())->method('getEndHour')->willReturn($endHour);
     }
 }
---
+++ tests/Component/Attendance/Service/AttendanceStub.php
@@ -0,0 +1,287 @@
+<?php
+
+namespace Tests\KejawenLab\Application\SemartHris\Component\Attendance\Service;
+use KejawenLab\Application\SemartHris\Component\Attendance\Model\AttendanceInterface;
+use KejawenLab\Application\SemartHris\Component\Attendance\Model\ShiftmentInterface;
+use KejawenLab\Application\SemartHris\Component\Employee\Model\EmployeeInterface;
+use KejawenLab\Application\SemartHris\Component\Reason\Model\ReasonInterface;
+
+/**
+ * @author Muhamad Surya Iksanudin <surya.iksanudin@kejawenlab.com>
+ */
+class AttendanceStub implements AttendanceInterface
+{
+    /**
+     * @var string
+     */
+    private $id;
+
+    /**
+     * @var EmployeeInterface
+     */
+    private $employee;
+
+    /**
+     * @var ShiftmentInterface
+     */
+    private $shiftment;
+
+    /**
+     * @var \DateTimeInterface
+     */
+    private $attendanceDate;
+
+    /**
+     * @var string
+     */
+    private $description;
+
+    /**
+     * @var \DateTimeInterface
+     */
+    private $checkIn;
+
+    /**
+     * @var \DateTimeInterface
+     */
+    private $checkOut;
+
+    /**
+     * @var int
+     */
+    private $earlyIn;
+
+    /**
+     * @var int
+     */
+    private $earlyOut;
+
+    /**
+     * @var int
+     */
+    private $lateIn;
+
+    /**
+     * @var int
+     */
+    private $lateOut;
+
+    /**
+     * @var bool
+     */
+    private $absent;
+
+    /**
+     * @var ReasonInterface
+     */
+    private $reason;
+
+    public function __construct()
+    {
+        $this->earlyIn = 0;
+        $this->earlyOut = 0;
+        $this->lateIn = 0;
+        $this->lateOut = 0;
+        $this->absent = false;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return (string) $this->id;
+    }
+
+    /**
+     * @return EmployeeInterface|null
+     */
+    public function getEmployee(): ? EmployeeInterface
+    {
+        return $this->employee;
+    }
+
+    /**
+     * @param EmployeeInterface|null $employee
+     */
+    public function setEmployee(?EmployeeInterface $employee): void
+    {
+        $this->employee = $employee;
+    }
+
+    /**
+     * @return ShiftmentInterface|null
+     */
+    public function getShiftment(): ? ShiftmentInterface
+    {
+        return $this->shiftment;
+    }
+
+    /**
+     * @param ShiftmentInterface|null $shiftment
+     */
+    public function setShiftment(?ShiftmentInterface $shiftment): void
+    {
+        $this->shiftment = $shiftment;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getAttendanceDate(): ? \DateTimeInterface
+    {
+        return $this->attendanceDate;
+    }
+
+    /**
+     * @param \DateTimeInterface|null $attendanceDate
+     */
+    public function setAttendanceDate(?\DateTimeInterface $attendanceDate): void
+    {
+        $this->attendanceDate = $attendanceDate;
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getDescription(): ? string
+    {
+        return $this->description;
+    }
+
+    /**
+     * @param string $description
+     */
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getCheckIn(): ? \DateTimeInterface
+    {
+        return $this->checkIn;
+    }
+
+    /**
+     * @param \DateTimeInterface|null $checkIn
+     */
+    public function setCheckIn(?\DateTimeInterface $checkIn): void
+    {
+        $this->checkIn = $checkIn;
+    }
+
+    /**
+     * @return \DateTimeInterface|null
+     */
+    public function getCheckOut(): ? \DateTimeInterface
+    {
+        return $this->checkOut;
+    }
+
+    /**
+     * @param \DateTimeInterface|null $checkOut
+     */
+    public function setCheckOut(?\DateTimeInterface $checkOut): void
+    {
+        $this->checkOut = $checkOut;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEarlyIn(): int
+    {
+        return $this->earlyIn;
+    }
+
+    /**
+     * @param int $earlyIn
+     */
+    public function setEarlyIn(int $earlyIn): void
+    {
+        $this->earlyIn = $earlyIn;
+    }
+
+    /**
+     * @return int
+     */
+    public function getEarlyOut(): int
+    {
+        return $this->earlyOut;
+    }
+
+    /**
+     * @param int $earlyOut
+     */
+    public function setEarlyOut(int $earlyOut): void
+    {
+        $this->earlyOut = $earlyOut;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLateIn(): int
+    {
+        return $this->lateIn;
+    }
+
+    /**
+     * @param int $lateIn
+     */
+    public function setLateIn(int $lateIn): void
+    {
+        $this->lateIn = $lateIn;
+    }
+
+    /**
+     * @return int
+     */
+    public function getLateOut(): int
+    {
+        return $this->lateOut;
+    }
+
+    /**
+     * @param int $lateOut
+     */
+    public function setLateOut(int $lateOut): void
+    {
+        $this->lateOut = $lateOut;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isAbsent(): bool
+    {
+        return (bool) $this->absent;
+    }
+
+    /**
+     * @param bool $absent
+     */
+    public function setAbsent(bool $absent): void
+    {
+        $this->absent = $absent;
+    }
+
+    /**
+     * @return ReasonInterface|null
+     */
+    public function getReason(): ? ReasonInterface
+    {
+        return $this->reason;
+    }
+
+    /**
+     * @param ReasonInterface $reason
+     */
+    public function setReason(?ReasonInterface $reason): void
+    {
+        $this->reason = $reason;
+    }
+}
